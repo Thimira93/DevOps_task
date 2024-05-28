4ece25dccac03e856a9d7c41c53f16761f82b2d8
Thimira93 <thimira.work@gmail.com>
2024-05-28 10:06:53 +0000

    check

--- Terraform-Copy/main.tf
+++ Terraform-Copy/main.tf
@@ -52,7 +52,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id = aws_subnet.app_subnet[0].id
+  subnet_id = aws_subnet.public_subnet[0].id
   
 }
 
